2e62efa190951655334faad5f91ed58fd2139f76
6588032Phudis <87897542+MarT1AZ@users.noreply.github.com>
2024-11-23 19:01:51 +0700

    Update Server.js to run on AWS

--- Server.js
+++ Server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const port = 8000
 
 app.get('/', (req, res) => {
-   res.sendFile('index.html')
+   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(3000,() => console.log("listening to port 3000"))
+app.listen(port ,() => console.log("listening to port " + port))
